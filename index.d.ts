20e96f875f06c318212dfaf2cc84c01770afa34b
A-Akash2000 <akashswamy62000@gmail.com>
2025-07-19 18:55:14 +0530

    User testcase completed

--- index.d.ts
+++ index.d.ts
@@ -8,19 +8,10 @@ declare global {
         id?: string;
         username?: string;
         email?: string;
-        contactNumber?: string;
         password?: string;
         role?: string;
-        shiftStart?: string;
-        shiftEnd?: string;
-        address?: string;
-        gender?: string;
-        isAvailable?: boolean;
-        lastUpdated?: Date;
         createdAt?: Date;
         updatedAt?: Date;
-        deleted?: boolean;
-        profilePicture?:String
       };
     }
   }
--- src/middlewares/middleware.ts
+++ src/middlewares/middleware.ts
@@ -10,18 +10,8 @@ interface AuthenticatedUser {
   id:string;
   username?: string;
   email?: string;
-  contactNumber?: string;
   password?: string;
   role?: string;
-  shiftStart?: string;
-  shiftEnd?: string;
-  address?: string;
-  gender?: string;
-  isAvailable?: boolean;
-  lastUpdated?: Date;
-  createdAt?: Date;
-  updatedAt?: Date;
-  deleted?: boolean;
 }
 
 declare module 'express-serve-static-core' {
